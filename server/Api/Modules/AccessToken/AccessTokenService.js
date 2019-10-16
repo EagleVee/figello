d5b2df48bb5a7587d9ce2480adf04ac3f041841c
EagleVee <nnavu1@gmail.com>
2019-10-16 21:04:25 +0700

    fix access token service

--- server/Api/Modules/AccessToken/AccessTokenService.js
+++ server/Api/Modules/AccessToken/AccessTokenService.js
@@ -1,45 +1,7 @@
-import Repository from './UserRepository'
-
-const find = async (query) => {
-  const data = await Repository.find(query)
-  const total = await Repository.count(query)
-  return {
-    data: data
-  }
-}
-
-const findById = async (id) => {
-  return Repository.findById(id)
-}
-
-const create = async (data) => {
-  return Repository.create(data)
-}
-
-const update = async function (id, data) {
-  const existedRecord = await Repository.findById(id)
-  if (!existedRecord) {
-    throw new Error('Entity not found!')
-  }
-
-  return Repository.update(id, data)
-}
-
-const deleteByID = async (id) => {
-  const existedRecord = await Repository.findById(id)
-  if (!existedRecord) {
-    throw new Error('Entity not found!')
-  }
-
-  return Repository.delete(id)
-}
+import Repository from './AccessTokenRepository'
 
 const service = {
-  find,
-  findById,
-  create,
-  update,
-  deleteByID
+
 }
 
 export default service
