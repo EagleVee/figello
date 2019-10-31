a29a139a31696a6a6f3d5c8250f248771a774e87
Ngoc Pham <ngocpham@techkids.io>
2019-10-31 22:36:18 +0700

    Implement board screen

---
+++ src/Containers/Boards.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "antd/dist/antd.css";
+import "./Styles/Board.css";
+import NavBar from "../Components/NavBar/NavBar";
+import { connect } from "react-redux";
+import { Card, Avatar } from "antd";
+
+class Board extends Component {
+  state = {
+    data: ['brown', '#a77858', 'blue', 'green', 'purple', 'orange', '#af3990']
+  };
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="container wrapper">
+          <div className="header">
+            <i className='fa fa-user fa-2x mr-3' />
+            <h3 className="headline">Personal boards</h3>
+          </div>
+          <div className="row">
+            {this.state.data.map(val => {
+              return (
+                <div className="col-lg-3 col-md-4 col-sm-6 card-container">
+                  <Card className='card card-light' loading={false} style={{backgroundColor: val}}>
+                    <Card.Meta
+                      title="Card title"
+                      description="This is the description"
+                    />
+                  </Card>
+                </div>
+              );
+            })}
+            <div className="col-lg-3 col-md-4 col-sm-6 card-container">
+              <Card className='card card-dark' loading={false} style={{backgroundColor: '#ddd', minHeight: '100%'}}>
+                <div className='vertical-center horizontal-center'>
+                  <p className='text-center mb-0' style={{fontSize: '18px'}}>Create new board</p>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Board);
--- src/Navigation/Routes.js
+++ src/Navigation/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import HomePage from '../Containers/HomePage'
 import LoginPage from '../Containers/LoginPage'
 import RegisterPage from '../Containers/RegisterPage'
+import Board from '../Containers/Boards'
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 export default (
@@ -9,5 +10,6 @@ export default (
     <Route exact path='/' component={HomePage} />
     <Route exact path='/login' component={LoginPage} />
     <Route exact path='/register' component={RegisterPage} />
+    <Route exact path='/board' component={Board} />
   </Router>
 )
