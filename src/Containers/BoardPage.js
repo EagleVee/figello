1f460d94671a47d24a8170246f9f6f2c71d8955c
EagleVee <nnavu1@gmail.com>
2019-11-02 21:51:19 +0700

    add process env

---
+++ src/Containers/BoardPage.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './Styles/HomePage.css'
+import NavBar from '../Components/NavBar/NavBar'
+import { connect } from 'react-redux'
+
+class BoardPage extends Component {
+  render () {
+    return (
+      <div>
+        <NavBar />
+        <div className='home-header text-center row'>
+          <div className='align-self-center col-12'>
+            <h1 className='welcome text-white'>WELCOME TO FIGELLO</h1>
+            <h4 className='slogan text-white mt-2'>START YOUR PERSONAL TEAMWORK EXPERIENCE</h4>
+            <Link to='/login' className='get-started btn btn-light text-dark mt-5'>
+              GET STARTED
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoardPage)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import * as serviceWorker from './serviceWorker'
 import App from './App'
 import React from 'react'
 
+require('dotenv').config()
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
